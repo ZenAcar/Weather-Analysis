4164661a9fe17abdfde8126f8bf68156b5cc2ee1
Zen <60336625+ZenAcar@users.noreply.github.com>
2020-03-26 21:33:39 -0500

    deleted keys

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "15ea7c11842d342d1a1702e00cbf204b"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCYsIk47bqc5Z9gE4Fq6VAtaQoPPTLkcRI"
+g_key = ""
